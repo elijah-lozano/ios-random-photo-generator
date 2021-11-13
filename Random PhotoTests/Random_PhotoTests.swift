72b179c7b5faa580282309d54f4534b840b59c95
elijah-lozano <elijahlozano@hotmail.com>
2021-11-13 12:49:31 -0600

    Initial Commit

---
+++ Random PhotoTests/Random_PhotoTests.swift
@@ -0,0 +1,33 @@
+//
+//  Random_PhotoTests.swift
+//  Random PhotoTests
+//
+//  Created by Elijah Lozano on 11/13/21.
+//
+
+import XCTest
+@testable import Random_Photo
+
+class Random_PhotoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
